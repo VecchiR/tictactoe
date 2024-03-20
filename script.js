bc760fd51d65ce134c01ff022e29e981f8c16970
Vecchi <79600745+VecchiR@users.noreply.github.com>
2024-03-20 16:51:24 -0300

    Enable player to select board space to write + change turns after play

--- script.js
+++ script.js
@@ -1,11 +1,52 @@
 const gameboard = ( function () {
     let board = [['','',''],['','',''],['','','']];
     const getBoard = () => {
-        console.log(board);
+        return board;
     }
-    return {getBoard};
+
+    const writeOnBoard = (x,y) => {
+        board[x][y] = gameflow.activePlayer().getMarker();
+        console.log(getBoard());
+    }
+
+    return {getBoard, writeOnBoard};
 
 })();
     
-gameboard.getBoard();
 
+function createPlayer (name, marker) {
+    const getMarker = () => {
+        return marker;
+    }
+    return {name, getMarker};
+}
+
+const gameflow = ( function () {
+
+    let playerTurn = 1;
+
+    const activePlayer = () => {
+        if (playerTurn === 1) {
+            playerTurn = 2;
+            return player1;
+        }
+        else {
+            playerTurn = 1;
+            return player2;
+        }
+    }
+
+    const selectSpace = (x,y) => {
+        return gameboard.writeOnBoard(x,y);
+    }
+
+    return {selectSpace, activePlayer};
+
+})();
+
+
+const player1 = createPlayer('Jimothy', 'x');
+const player2 = createPlayer('Aroldo', 'o');
+
+console.log(gameboard.getBoard());
+console.log([player1.name, player1.getMarker()], [player2.name, player2.getMarker()]);
